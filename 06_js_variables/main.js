577d057058ac6bc3078057c91efb4e61f092ad4f
Pia Feiel <pia@Pias-Air.fritz.box>
2020-03-22 09:59:25 +0100

    Aufgabe 9

--- 06_js_variables/main.js
+++ 06_js_variables/main.js
@@ -1,3 +1,5 @@
-const myName = "Hugo"
-let myCity = "Washington"
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);
